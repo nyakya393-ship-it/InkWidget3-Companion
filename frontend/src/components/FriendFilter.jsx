1e300a2eddc52f96d6edb778dcf239df8123ee5c
Sio <nyakya393@gmail.com>
2026-07-09 16:20:27 +0900

    Update FriendFilter.jsx

--- frontend/src/components/FriendFilter.jsx
+++ frontend/src/components/FriendFilter.jsx
@@ -1,1 +1,64 @@
+import React from "react";
 
+
+function FriendFilter(){
+
+
+    const filters = [
+
+        "すべて",
+
+        "オンライン",
+
+        "試合中",
+
+        "オフライン"
+
+    ];
+
+
+
+    return (
+
+        <div className="friend-filter">
+
+
+            {
+
+                filters.map((filter,index)=>(
+
+
+                    <button
+
+                        className={
+
+                            index === 0
+
+                            ? "filter-pill active"
+
+                            : "filter-pill"
+
+                        }
+
+                        key={index}
+
+                    >
+
+                        {filter}
+
+                    </button>
+
+
+                ))
+
+            }
+
+
+        </div>
+
+    );
+
+}
+
+
+export default FriendFilter;
