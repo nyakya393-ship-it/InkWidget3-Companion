b4fa7f4530fe17a7d3601ff0f1ca492f4e103c26
Sio <nyakya393@gmail.com>
2026-07-09 12:26:44 +0900

    Update Home.jsx

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,1 +1,53 @@
+import React from "react";
 
+import CurrentCard from "../components/CurrentCard";
+import ScheduleTimeline from "../components/ScheduleTimeline";
+import EventMatchCard from "../components/EventMatchCard";
+import FestivalCard from "../components/FestivalCard";
+import GearShopCard from "../components/GearShopCard";
+
+
+function Home(){
+
+    return (
+
+        <main className="home">
+
+
+            {/* 現在の状況 */}
+
+            <CurrentCard />
+
+
+
+            {/* 24時間スケジュール */}
+
+            <ScheduleTimeline />
+
+
+
+            {/* イベントマッチ */}
+
+            <EventMatchCard />
+
+
+
+            {/* フェス */}
+
+            <FestivalCard />
+
+
+
+            {/* ゲソタウン */}
+
+            <GearShopCard />
+
+
+        </main>
+
+    );
+
+}
+
+
+export default Home;
