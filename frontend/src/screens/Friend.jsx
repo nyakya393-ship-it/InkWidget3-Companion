8925dd5db9fb8af9d2917bf5a325379a7759863b
Sio <nyakya393@gmail.com>
2026-07-09 16:18:48 +0900

    Update Friend.jsx

--- frontend/src/screens/Friend.jsx
+++ frontend/src/screens/Friend.jsx
@@ -1,1 +1,105 @@
+import React from "react";
 
+import FriendFilter from "../components/FriendFilter";
+import FriendCard from "../components/FriendCard";
+import FriendDetail from "../components/FriendDetail";
+
+
+function Friends(){
+
+
+    const friends = [
+
+        {
+
+            name:"かなと",
+
+            status:"試合中",
+
+            mode:"バンカラマッチ",
+
+            rule:"オープン",
+
+            stage:"ザトウマーケット"
+
+        },
+
+        {
+
+            name:"あいう",
+
+            status:"オンライン",
+
+            mode:"ロビー",
+
+            rule:"",
+
+            stage:""
+
+        },
+
+        {
+
+            name:"うえお",
+
+            status:"オフライン",
+
+            mode:"",
+
+            rule:"",
+
+            stage:""
+
+        }
+
+    ];
+
+
+
+    return (
+
+        <main className="friends-page">
+
+
+            <FriendFilter />
+
+
+
+            <section className="friend-list">
+
+
+                {
+
+                    friends.map((friend,index)=>(
+
+
+                        <FriendCard
+
+                            key={index}
+
+                            friend={friend}
+
+                        />
+
+
+                    ))
+
+                }
+
+
+            </section>
+
+
+
+            <FriendDetail />
+
+
+        </main>
+
+    );
+
+}
+
+
+
+export default Friends;
