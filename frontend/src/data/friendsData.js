9bd4990a42a1c7e587e15115a4828b43cf70b955
Sio <nyakya393@gmail.com>
2026-07-09 16:58:53 +0900

    Create friendsData.js

---
+++ frontend/src/data/friendsData.js
@@ -0,0 +1,44 @@
+const friendsData = [
+
+    {
+        name:"かなと",
+
+        status:"試合中",
+
+        mode:"バンカラマッチ",
+
+        rule:"オープン",
+
+        stage:"ザトウマーケット"
+    },
+
+
+    {
+        name:"あいう",
+
+        status:"オンライン",
+
+        mode:"ロビー",
+
+        rule:"",
+
+        stage:""
+    },
+
+
+    {
+        name:"うえお",
+
+        status:"オフライン",
+
+        mode:"",
+
+        rule:"",
+
+        stage:""
+    }
+
+];
+
+
+export default friendsData;
